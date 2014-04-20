54c563375a94824c28901ce63e1fb59b522adfcb
Casper Lai <casper.lai@sleepingdesign.com>
2014-04-20 15:49:02 +0800

    Project initialize. Added more package.

---
+++ app/config/local/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'helloworld2',
+			'username'  => 'helloworld',
+			'password'  => 'helloworld@user',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+	),
+);
